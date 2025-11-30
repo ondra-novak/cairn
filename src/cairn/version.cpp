17f579004d53d74c33429d8b0b8b8478b32e8f8a
Ondřej Novák <ondra-novak@email.cz>
2025-11-30 10:59:36 +0100

    merge

--- src/cairn/version.cpp
+++ src/cairn/version.cpp
@@ -2,8 +2,4 @@ export module cairn.version;
 
 import <string_view>;
 
-<<<<<<< HEAD
-export constexpr std::string_view APP_VERSION = "fb2b3ff";
-=======
-export constexpr std::string_view APP_VERSION = "ea06ab2";
->>>>>>> 787117031882e77ed94c4090ba3cfb4671cb698a
+export constexpr std::string_view APP_VERSION = "78853ad";
